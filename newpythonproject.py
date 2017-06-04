9c199d58407aa294a33227e13a9b85b25ce246d2
test <test@gmail.com>
2017-06-04 23:32:26 +0900

    python

---
+++ newpythonproject.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "t-suzuki"
+__date__ = "$2017/06/04 16:08:16$"
+
+if __name__ == "__main__":
+    print "Hello World"
