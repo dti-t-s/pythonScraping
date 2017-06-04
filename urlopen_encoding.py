af45345a4dc424fa0fa4822f7389af44d4113731
test <test@gmail.com>
2017-06-05 00:02:02 +0900

    https://ja.stackoverflow.com/questions/27922/python3%E3%81%A7web%E3%82%B9%E3%82%AF%E3%83%AC%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0%E3%81%97%E3%81%9F%E3%81%84%E3%81%AE%E3%81%A7%E3%81%99%E3%81%8C%E5%AD%98%E5%9C%A8%E3%81%99%E3%82%8Burl%E3%81%8C%E9%96%8B%E3%81%91%E3%81%BE%E3%81%9B%E3%82%93

--- urlopen_encoding.py
+++ urlopen_encoding.py
@@ -1,8 +1,15 @@
 import sys
-from urllib.request import urlopen
+import urllib.request
+
+url = "https://gihyo.jp/dp/"
+headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0",
+        }
+
+request = urllib.request.Request(url=url, headers=headers)
 
 #f = urlopen('https://www.yahoo.co.jp/')
-f = urlopen('https://gihyo.jp/dp/')
+f = urllib.request.urlopen(request)
 # HTTPヘッダーからエンコーディングを取得する（明示されていない場合はutf-8とする）
 encoding = f.info().get_content_charset(failobj='utf-8')
 print('encoding:', encoding, file=sys.stderr) # エンコーディングを標準エラー出力に出力する
