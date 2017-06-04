7e3aabb2a6f428be5a734e81ab052a823eb4bc65
test <test@gmail.com>
2017-06-04 23:40:02 +0900

    no message

--- newpythonproject.py
+++ newpythonproject.py
@@ -2,7 +2,6 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-__author__ = "t-suzuki"
 __date__ = "$2017/06/04 16:08:16$"
 
 if __name__ == "__main__":
